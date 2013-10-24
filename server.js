19b32c038e3ede9020d29d86b1ee7a9a45fbc39d
Lee Whitaker <lwhitaker@brightcove.com>
2013-10-24 13:56:37 -0400

    Respond to REST methods not path names. some cleanup.

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ db.once('open', function() {
   positionSchema = mongoose.Schema({
     '_id': {type: String},
     'userId': {type: String},
-    'videoId': {type: Number},
+    'videoId': {type: String},
     'currentPosition': {type: Number},
     'date': {type: Date, default: Date.now }
   });
@@ -27,15 +27,12 @@ db.once('open', function() {
   console.log('teleport is listening on port: ' + appPort);
 });
 
-app.get('/set/:userId/:videoId/:currentPosition', function (req, res) {
-  //res.set('Access-Control-Allow-Origin','http://ec2-107-20-72-18.compute-1.amazonaws.com');
+app.post('/:userId/:videoId/:currentPosition', function (req, res) {
   res.set('Access-Control-Allow-Origin', '*');
   var 
     userId = req.params.userId,
-    videoId = parseInt(req.params.videoId),
-    currentPosition = parseInt(req.params.currentPosition),
-
-    position;
+    videoId = req.params.videoId,
+    currentPosition = parseInt(req.params.currentPosition);
 
   if (!userId || !videoId || !currentPosition) {
     res.json(412, {
@@ -51,7 +48,7 @@ app.get('/set/:userId/:videoId/:currentPosition', function (req, res) {
       currentPosition: currentPosition, 
       date: new Date()}}, 
     {upsert: true}, 
-    function (err, position) {
+    function (err) {
       if (err) {
         res.send('Error: ', err);
       }
@@ -59,36 +56,7 @@ app.get('/set/:userId/:videoId/:currentPosition', function (req, res) {
   });
 });
 
-app.get('/delete/:userId/:videoId', function (req, res) {
-  //res.set('Access-Control-Allow-Origin','http://ec2-107-20-72-18.compute-1.amazonaws.com');
-  res.set('Access-Control-Allow-Origin', '*');
-  var 
-    userId = req.params.userId,
-    videoId = parseInt(req.params.videoId),
-    currentPosition = parseInt(req.params.currentPosition),
-
-    position;
-
-  if (!userId || !videoId) {
-    res.json(412, {
-      errorCode: 'MissingRequiredParam',
-      errorMessage: 'You must provide a valid userId, videoId, and position in the request.'
-    });  
-    return;
-  }
-  
-  Position.remove(
-    {userId: userId, videoId: videoId}, 
-    function (err, position) {
-      if (err) {
-        res.send('Error: ', err);
-      }
-      res.send('Deleted!');
-  });
-});
-
-app.get('/get/:userId/:videoId', function (req, res) {
-  //res.set('Access-Control-Allow-Origin','http://ec2-107-20-72-18.compute-1.amazonaws.com');
+app.get('/:userId/:videoId', function (req, res) {
   res.set('Access-Control-Allow-Origin', '*');
   var 
     userId = req.params.userId,
@@ -102,10 +70,40 @@ app.get('/get/:userId/:videoId', function (req, res) {
     return;
   }
   Position.find({userId: userId, videoId: videoId}, function (err, results) {
-    if (results[0] && results[0].currentPosition > 0 ) {
+    if (results && results[0] && results[0].currentPosition > 0 ) {
       res.send(results[0].currentPosition.toString());
     } else {
-      res.send('0')
+      res.send('0');
     }
+  });
+});
+
+app.options('*', function (req,res) {
+  res.set('Access-Control-Allow-Origin', '*');
+  res.set('Access-Control-Allow-Methods:','GET,POST,DELETE,OPTIONS');
+  res.send();
+});
+
+app.del('/:userId/:videoId', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
+  var 
+    userId = req.params.userId,
+    videoId = req.params.videoId;
+
+  if (!userId || !videoId) {
+    res.json(412, {
+      errorCode: 'MissingRequiredParam',
+      errorMessage: 'You must provide a valid userId, videoId, and position in the request.'
+    });  
+    return;
+  }
+  
+  Position.remove(
+    {userId: userId, videoId: videoId}, 
+    function (err) {
+      if (err) {
+        res.send('Error: ', err);
+      }
+      res.send('Deleted!');
   });
 });

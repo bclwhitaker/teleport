506ba979611b68b8ebe3b9fbf620bfa43a213553
Stewart Glass <sglass@brightcove.com>
2014-07-15 11:17:15 -0400

    Adding in the playlist collection

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 teleport
 ========
+To update a time:
+curl -XPOST http://{$ADDRESS}/userId/:userId/videoId/:videoId/position/:currentPosition
+
+To update where in a playlist the user is:
+curl -XPOST http://{$ADDRESS}/userId/:userId/playlistId/:playlistId/videoId/:videoId
+
+To get a time:
+curl -XGET http://{$ADDRESS}/userId/:userId/videoId/:videoId
+
+To get where a user is in the playlist:
+curl -XGET http://{$ADDRESS}/userId/:userId/playlistId/:playlistId
+
+To delete a time entry:
+curl -XDELETE http://{$ADDRESS}/userId/:userId/videoId/:videoId
--- server.js
+++ server.js
@@ -1,11 +1,13 @@
-var 
+var
   express = require('express'),
   mongoose = require('mongoose'),
+  crypto = require('crypto'),
 
   app = express(),
-  appPort = 80,
+  appPort = 2005,
 
   positionSchema,
+  Playlist,
   Position;
 
 app.use(express.compress());
@@ -22,19 +24,35 @@ db.once('open', function() {
     'currentPosition': {type: Number},
     'date': {type: Date, default: Date.now }
   });
+
+  playlistResumeSchema = mongoose.Schema({
+    '_id': {type: String},
+    'playlistHash': {type: String},
+    'currentVideo': {type: String},
+    'date': {type: Date, default: Date.now}
+  });
+
   Position = mongoose.model('Positions', positionSchema);
+
+  // The collection that tracks where the user is in a specific playlist.
+  Playlist = mongoose.model('Playlists', playlistResumeSchema);
+
   app.listen(appPort);
   console.log('teleport is listening on port: ' + appPort);
 });
 
-app.post('/:userId/:videoId/:currentPosition', function (req, res) {
+/**
+* Handles updating the Mongo collection which tracks the position users are at in specific
+* videos.
+*/
+app.post('/userId/:userId/videoId/:videoId/position/:position', function (req, res) {
   res.set('Access-Control-Allow-Origin', '*');
   var 
     userId = req.params.userId,
     videoId = req.params.videoId,
-    currentPosition = parseInt(req.params.currentPosition);
+    currentPosition = parseInt(req.params.position);
 
-  if (!userId || !videoId || !currentPosition) {
+  if (!userId ||!videoId || !currentPosition) {
     res.json(412, {
       errorCode: 'MissingRequiredParam',
       errorMessage: 'You must provide a valid userId, videoId, and position in the request.'
@@ -45,7 +63,7 @@ app.post('/:userId/:videoId/:currentPosition', function (req, res) {
   Position.update(
     {userId: userId, videoId: videoId}, 
     {$set: {
-      currentPosition: currentPosition, 
+      currentPosition: currentPosition,
       date: new Date()}}, 
     {upsert: true}, 
     function (err) {
@@ -56,7 +74,57 @@ app.post('/:userId/:videoId/:currentPosition', function (req, res) {
   });
 });
 
-app.get('/:userId/:videoId', function (req, res) {
+/**
+* Used to update the tracker that follows where a user is in a specific
+* playlist. The tracking is done by looking up the hash of the userId and playlistId
+* and finding the corresponding videoId which is where the user left off.
+*/
+app.post('/userId/:userId/playlistId/:playlistId/videoId/:videoId', function (req, res) {
+  var
+    userId = req.params.userId,
+    videoId = req.params.videoId,
+    playlistId = req.params.playlistId,
+    playlistHash;
+
+  res.set('Access-Control-Allow-Origin', '*');
+
+  if (!userId || !playlistId || !videoId) {
+    res.json(412, {
+      errorCode: 'MissingRequiredParam',
+      errorMessage: 'You must provide a valid userId, videoId and playlistId in the request.'
+    });
+    return;
+  }
+
+  // Generate the hash of the userId + playlistId.
+  playlistHash = crypto.createHash('md5').update(userId + playlistId).digest('hex');
+
+  // Update the collection with the new video Id.
+  Playlist.update(
+    {playlistHash: playlistHash, currentVideo: videoId},
+    {
+      $set: {
+        playlistHash: playlistHash,
+        currentVideo: videoId
+      }
+    },
+    {upsert: true},
+    function (err) {
+      if (err) {
+        res.send('Error: ', err);
+      }
+      res.send('Success!');
+    }
+  );
+});
+
+/**
+* Responds with where the user should be in the specified video. If there is no entry
+* in the database for this user/video 0 is returned.
+*
+* Numbers returned are in seconds.
+*/
+app.get('/userId/:userId/videoId/:videoId', function (req, res) {
   res.set('Access-Control-Allow-Origin', '*');
   var 
     userId = req.params.userId,
@@ -78,13 +146,48 @@ app.get('/:userId/:videoId', function (req, res) {
   });
 });
 
+/**
+* Responds with a video ID corresponding to where in the provided playlist the
+* specified user should be.
+*/
+app.get('/userId/:userId/playlistId/:playlistId', function(req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
+
+  var
+    userId = req.params.userId,
+    playlistId = req.params.playlistId,
+    playlistHash;
+
+  if (!userId || !playlistId) {
+    res.json(412, {
+      errorCode: 'MissingRequiredParam',
+      errorMessage: 'You must provide a valid userId and playlistId in the request.'
+    });
+    return;
+  }
+
+  // Generate the hash of the userId + playlistId.
+  playlistHash = crypto.createHash('md5').update(userId + playlistId).digest('hex');
+
+  Playlist.find({playlistHash: playlistHash}, function(err, results) {
+    if (results && results[0] && results[0].currentVideo) {
+      res.send(results[0].currentVideo.toString());
+    } else {
+      res.send('0');
+    }
+  });
+});
+
 app.options('*', function (req,res) {
   res.set('Access-Control-Allow-Origin', '*');
   res.set('Access-Control-Allow-Methods:','GET,POST,DELETE,OPTIONS');
   res.send();
 });
 
-app.del('/:userId/:videoId', function (req, res) {
+/**
+* Removes the time entry for the specified user ID and video ID combo.
+*/
+app.del('/userId/:userId/videoId/:videoId', function (req, res) {
   res.set('Access-Control-Allow-Origin', '*');
   var 
     userId = req.params.userId,
@@ -94,12 +197,42 @@ app.del('/:userId/:videoId', function (req, res) {
     res.json(412, {
       errorCode: 'MissingRequiredParam',
       errorMessage: 'You must provide a valid userId, videoId, and position in the request.'
-    });  
+    });
     return;
   }
   
   Position.remove(
     {userId: userId, videoId: videoId}, 
+    function (err) {
+      if (err) {
+        res.send('Error: ', err);
+      }
+      res.send('Deleted!');
+  });
+});
+
+/**
+* Removes the entry in the playlist progress tracking collection
+* for the specified user ID and playlist ID combo.
+*/
+app.del('/userId/:userId/playlistId/:playlistId', function (req, res) {
+  res.set('Access-Control-Allow-Origin', '*');
+  var
+    userId = req.params.userId,
+    playlistId = req.params.playlistId;
+  // Generate the hash of the userId + playlistId.
+  playlistHash = crypto.createHash('md5').update(userId + playlistId).digest('hex');
+
+  if (!userId || !playlistId) {
+    res.json(412, {
+      errorCode: 'MissingRequiredParam',
+      errorMessage: 'You must provide a valid userId and playlistId in the request.'
+    });
+    return;
+  }
+
+  Playlist.remove(
+    {playlistHash: playlistHash},
     function (err) {
       if (err) {
         res.send('Error: ', err);
